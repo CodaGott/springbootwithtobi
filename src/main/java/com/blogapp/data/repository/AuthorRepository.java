f75816e299afb166071de1c41be9ec269790c589
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-05-27 14:40:16 +0100

    add repositories

---
+++ src/main/java/com/blogapp/data/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.blogapp.data.repository;
+
+import com.blogapp.data.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
---
+++ src/main/java/com/blogapp/data/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.blogapp.data.repository;
+
+import com.blogapp.data.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+}
