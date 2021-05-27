7fa99b406fbc8ae4bbb75ccbb353fd7d967b36c4
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-05-27 11:50:56 +0100

    test we can connect to database

---
+++ src/main/java/com/blogapp/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.blogapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public @ResponseBody String showWelcome(){
+        return "Welcome pentax";
+    }
+}
