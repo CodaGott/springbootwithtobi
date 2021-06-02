0f2a1008b2c7f9fefd249781b13f2158d529c1a5
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-06-02 17:08:01 +0100

    update

--- src/main/java/com/blogapp/web/controllers/PostController.java
+++ src/main/java/com/blogapp/web/controllers/PostController.java
@@ -6,6 +6,7 @@ import com.blogapp.web.dto.PostDTO;
 import com.blogapp.web.exceptions.PostObjectIsNullException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,18 +35,24 @@ public class PostController {
 
     @GetMapping("/create")
     public String getPostForm(Model model){
-        model.addAttribute("post", new PostDTO());
+        model.addAttribute("postDTO", new PostDTO());
+        model.addAttribute("error", false);
         return "create";
     }
 
     @PostMapping("/save")
-    public String savePost(@ModelAttribute @Valid PostDTO postDTO){
+    public String savePost(@ModelAttribute @Valid PostDTO postDTO, Model model){
         log.info("Post dto received --> {}", postDTO);
 
         try{
             postServiceImpl.savePost(postDTO);
         }catch (PostObjectIsNullException pe){
-            log.info("Exception occured --> {}", pe.getMessage());
+            log.info("Exception occurred --> {}", pe.getMessage());
+        }catch (DataIntegrityViolationException dx){
+            log.info("Constrain exception occurred --> {}", dx.getMessage());
+            model.addAttribute("error", true);
+            model.addAttribute("errorMessage", dx.getMessage());
+            return "create";
         }
         return "redirect:/posts";
     }
